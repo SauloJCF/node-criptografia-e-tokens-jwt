b49c4e578a1bfd59a5316f4cfc0fe379fa68a6fa
Saulo Jose Coelho de Figueiredo <saulojcf@gmail.com>
2025-08-03 20:46:30 -0300

    Refatora cifra de cesar

--- cifra.js
+++ cifra.js
@@ -1,31 +1,51 @@
-const minhaMensagem = 'minhamensagemsecreta';
+const minhaMensagem = "minha mensagem secreta zumba";
 
 console.log(minhaMensagem);
 
 function cifraMensagem(mensagem, movimentos) {
-    const mensagemCifrada = mensagem.split('').map(caractere => {
-        const codigoCaractere = caractere.charCodeAt(0);
+  const mensagemCifrada = mensagem.split("").map((caractere) => {
+    const codigoCaractere = caractere.charCodeAt(0);
 
-        return String.fromCharCode(codigoCaractere + movimentos);
-    });
+    let codigoCaracterCifra;
 
-    return mensagemCifrada.join('');
+    if (codigoCaractere >= 65 && codigoCaractere <= 90) {
+      codigoCaracterCifra = ((codigoCaractere - 65 + movimentos) % 26) + 65;
+    } else if (codigoCaractere >= 97 && codigoCaractere <= 122) {
+      codigoCaracterCifra = ((codigoCaractere - 97 + movimentos) % 26) + 97;
+    } else {
+      codigoCaracterCifra = codigoCaractere;
+    }
+
+    return String.fromCharCode(codigoCaracterCifra);
+  });
+
+  return mensagemCifrada.join("");
 }
 
-const mensagemCifrada = cifraMensagem(minhaMensagem, 3);
+const mensagemCifrada = cifraMensagem(minhaMensagem, 6);
 
 console.log(mensagemCifrada);
 
 function decifraMensagem(mensagem, movimentos) {
-    const mensagemDeCifrada = mensagem.split('').map(caractere => {
-        const codigoCaractere = caractere.charCodeAt(0);
+  const mensagemDeCifrada = mensagem.split("").map((caractere) => {
+    const codigoCaractere = caractere.charCodeAt(0);
 
-        return String.fromCharCode(codigoCaractere - movimentos);
-    });
+    let codigoCaracterDecifrado;
 
-    return mensagemDeCifrada.join('');
+    if (codigoCaractere >= 65 && codigoCaractere <= 90) {
+      codigoCaracterDecifrado = ((codigoCaractere - 65 - movimentos + 26) % 26) + 65;
+    } else if (codigoCaractere >= 97 && codigoCaractere <= 122) {
+      codigoCaracterDecifrado = ((codigoCaractere - 97 - movimentos + 26) % 26) + 97;
+    } else {
+      codigoCaracterDecifrado = codigoCaractere;
+    }
+
+    return String.fromCharCode(codigoCaracterDecifrado);
+  });
+
+  return mensagemDeCifrada.join("");
 }
 
-const mensagemDecifrada = decifraMensagem(mensagemCifrada, 3);
+const mensagemDecifrada = decifraMensagem(mensagemCifrada, 6);
 
 console.log(mensagemDecifrada);
